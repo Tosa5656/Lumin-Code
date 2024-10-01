04d960918f066b01dad4a11a9a1a210fdd8a5712
Tosa <127298458+Tosa5656@users.noreply.github.com>
2024-10-01 19:35:25 +0400

    Added code part and comment search and set color

--- main.py
+++ main.py
@@ -3,11 +3,46 @@ import ctypes
 import re
 import os
 
+previousText = ""
+keys = ["void", "int", "bool"]
+
 def rgb(rgb):
     return "#%02x%02x%02x" % rgb
 
+def onUpdateSyntax(event):
+    global previousText
+    
+    lines = editArea.get("1.0", END).splitlines()
+    
+    if previousText == editArea.get("1.0", END):
+        return None
+    
+    editArea.tag_delete("code")
+    editArea.tag_delete("comment")
+    editArea.tag_configure("code", foreground=keywords)
+    editArea.tag_configure("comment", foreground=comments)
+    
+    # Set code colors
+    i = 1
+    for line in lines:
+        pattern = r"\b(" + "|".join(keys) + r")\b"
+        matches = [(match, line.find(match)) for match in re.findall(pattern, line)]
+        for match, pos in matches:
+            editArea.tag_add("code", str(i) + "." + str(pos), str(i) + "." + str(pos + len(match)))
+        
+        if line.find("//") != -1:
+            start_pos = re.search("//", line).start()
+            end_pos = start_pos + len(line)
+            if start_pos != -1:
+                editArea.tag_add("comment", str(i) + "." + str(start_pos), str(i) + "." + str(end_pos))
+    
+        i += 1
+    
+    previousText = editArea.get("1.0", END)
+
 def onEditText(event):
-    pass
+    onUpdateSyntax(event)
+        
 
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
@@ -16,8 +51,6 @@ root_window = Tk()
 root_window.title("Lumin Code")
 root_window.geometry("1280x720")
 
-
-previousText = ""
 
 normal = rgb((234, 234, 234))
 keywords = rgb((234, 95, 95))
@@ -33,11 +66,11 @@ editArea = Text(
 )
 
 editArea.pack(fill=BOTH, expand=1)
+editArea.config(tabs="1.2c")
 editArea.insert("1.0", "// Welcome to Lumin Code! \n" +
                 "// Start write your code.")
 
-editArea.tag_configure("code", foreground=keywords)
-
 editArea.bind("<KeyRelease>", onEditText)
+editArea.bind("<Control-l>", onUpdateSyntax)
 
 root_window.mainloop()
